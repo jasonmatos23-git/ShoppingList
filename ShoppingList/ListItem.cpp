545c5ee71becd7fd57bb8ec81024bedc6b56f7f2
Cristopher Matos <jasonmatos23@gmail.com>
2023-01-28 12:43:29 -0500

    Adds ListDescriptor class to header and moves name and description members

--- ShoppingList/ListItem.cpp
+++ ShoppingList/ListItem.cpp
@@ -64,99 +64,9 @@ ListItem::~ListItem(void)
 	free(this->m_description);
 }
 
-// -------
-// Helpers
-// -------
-char* ListItem::setString(char** m_str, const char* i_str, int length)
-{
-	if (i_str == nullptr)
-	{
-		free(*m_str);
-		*m_str = nullptr;
-		return nullptr;
-	}
-	else
-	{
-		if (length < 1)
-		{
-			throw new std::invalid_argument("ListItem->string length should not be less than 1.");
-		}
-		else
-		{
-			*m_str = static_cast<char*>(malloc(length * sizeof(char)));
-			if (*m_str == nullptr)
-			{
-				throw new std::bad_alloc;
-			}
-			else
-			{
-				memcpy(*m_str, i_str, length);
-				return *m_str;
-			}
-		}
-	}
-}
-
-char* ListItem::setString(char** m_str, const char* i_str)
-{
-	int length{ 0 };
-	if (i_str != nullptr)
-	{
-		while (i_str[length++] != '\0') {}
-	}
-	return setString(m_str, i_str, length);
-}
-
 // -------------------
 // Getters and Setters
 // -------------------
-char* ListItem::getName(void)
-{
-	return this->m_name;
-}
-
-char* ListItem::setName(char* name)
-{
-	char* temp {this->m_name};
-	this->m_name = name;
-	return temp;
-}
-
-char* ListItem::setName(const char* name, int length)
-{
-	return setString(&this->m_name, name, length);
-}
-
-char* ListItem::setName(const char* name)
-{
-	return setString(&this->m_name, name);
-}
-
-
-
-char* ListItem::getDescription(void)
-{
-	return this->m_description;
-}
-
-char* ListItem::setDescription(char* description)
-{
-	char* temp {this->m_description};
-	this->m_description = description;
-	return temp;
-}
-
-char* ListItem::setDescription(const char* description, int length)
-{
-	return setString(&this->m_description, description, length);
-}
-
-char* ListItem::setDescription(const char* description)
-{
-	return setString(&this->m_description, description);
-}
-
-
 unsigned int ListItem::getQuantity(void)
 {
 	return this->m_quantity;
