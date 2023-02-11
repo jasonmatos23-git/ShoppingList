8ed561d7812beb3b62959a3bb37fab2bd63d4398
Cristopher Matos <jasonmatos23@gmail.com>
2023-02-11 16:15:39 -0500

    Formats list item printing

--- ShoppingList/List.cpp
+++ ShoppingList/List.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <iomanip>
 #include "ShoppingList.h"
 
 // ------------
@@ -34,10 +35,11 @@ void List::removeListItem(int index)
 void List::printListItem(unsigned int index, unsigned int filter)
 {
 	ListItem* li = this->m_listItems.at(index);
+	int kDefaultIndentWidth = 15;
 	std::cout << index << ": " << "\n";
 	if (filter & ListItem::Filter::NAME)
 	{
-		std::cout << "Name:\t";
+		std::cout << std::right << std::setfill(' ') << std::setw(kDefaultIndentWidth) << "Name:\t";
 		if (li->getName() != nullptr)
 		{
 			std::cout << li->getName();
@@ -46,7 +48,7 @@ void List::printListItem(unsigned int index, unsigned int filter)
 	}
 	if (filter & ListItem::Filter::DESCRIPTION)
 	{
-		std::cout << "Desc:\t";
+		std::cout << std::right << std::setfill(' ') << std::setw(kDefaultIndentWidth) << "Description:\t";
 		if (li->getDescription() != nullptr)
 		{
 			std::cout << li->getDescription();
@@ -55,18 +57,23 @@ void List::printListItem(unsigned int index, unsigned int filter)
 	}
 	if (filter & ListItem::Filter::QUANTITY)
 	{
-		std::cout << "Quantity:\t";
+		std::cout << std::right << std::setfill(' ') << std::setw(kDefaultIndentWidth) << "Quantity:\t";
 		std::cout << li->getQuantity() << "\n";
 	}
 	if (filter & ListItem::Filter::CATEGORY)
 	{
-		std::cout << "Category:\t";
+		std::cout << std::right << std::setfill(' ') << std::setw(kDefaultIndentWidth) << "Category:\t";
 		std::cout << li->getCategoryStr() << "\n";
 	}
 }
 
 void List::printListItems(unsigned int filter)
 {
+	std::cout << this->getName() << '\n';
+	if (this->getDescription() != nullptr)
+	{
+		std::cout << this->getDescription() << '\n';
+	}
 	for (unsigned int i = 0; i < this->m_listItems.size(); i++)
 	{
 		printListItem(i, filter);
